af6364da653ba24765b3b5847bbac3cb5ab3f07b
VinnyMaker <vinnymaker18@gmail.com>
2018-05-21 15:07:25 +0530

    WIKIT Use alarms to redirect user when limit reached on wiki page

--- gatekeeper.js
+++ gatekeeper.js
@@ -1,9 +1,9 @@
 /**
- * Extract the domain part of a url.
+ * Extract the host part of a url.
  */
-function getURLDomain(url) {
+function getURLHost(url) {
     if (url === undefined)
-        return false;
+        return "";
 
     if (url.startsWith("http://"))
         url = url.substring(7);
@@ -19,16 +19,40 @@ function getURLDomain(url) {
 
 // Check if the url is of wikipedia.org domain.
 function isWikipediaDomain(url) {
-    return getURLDomain(url).endsWith("wikipedia.org");
+    return getURLHost(url).endsWith("wikipedia.org");
+};
+
+// Set an object property to the given value, if it doesn't exist already. If
+// the object is undefined however, it's a nop
+function setValueIfNotExists(obj, key, val) {
+    if (obj === undefined)
+        return;
+    if (!(key in obj))
+        obj[key] = val;
 };
 
 // Returns a string containing full year, month and day. Such strings can be used as keys
 // for storing daywise browsing data in local storage.
-function getUTCDateKey() {
+function getDateKey() {
     var now = new Date();
-    return now.getUTCFullYear() + ":" + now.getUTCMonth() + ":" +
-        now.getUTCDate();
+    return now.getFullYear() + ":" + now.getMonth() + ":" + now.getDate();
 }
+
+// Return the time of next midnight.
+// when time = May 21, 11:59:59 PM -> return May 22, 12:00:00 AM
+// when time = May 21, 12:00:00 AM -> return May 22, 12:00:00 AM
+function getNextMidnightTime() {
+    var now = new Date();
+    // Clear out the hours, minutes and seconds fields.
+    now.setHours(0);
+    now.setMinutes(0);
+    now.setSeconds(0);
+
+    // Increment the day by 1(this will automatically increment the month, year
+    // fields if necessary)
+    now.setDate(now.getDate() + 1);
+    return now;
+};
 
 // Return a string describing the contents(key/values) of the passed object. Useful
 // for debugging purposes.
@@ -48,6 +72,7 @@ function getPropString(obj) {
     return ret;
 }
 
+// Return a string concatenating all the given array elements with the given delimiter.
 function arrayElemsToString(arr, delim) {
     if (delim === undefined)
         delim = " ";
@@ -86,26 +111,36 @@ function addTabEventListeners(processTabEvent) {
     });
 };
 
+// Clear entries of browsingData object with keys older than dateKey.
+function clearOlderEntries(browsingData, dateKey) {
+    for (var key in browsingData) {
+        if (key !== dateKey) {
+            delete browsingData[key];
+        }
+    }
+};
+
+// Read data from local storage and use it to initialize extension state.
 function initialize(storedData) {
-    var DAILY_TIME_LIMIT_MS = 300000;
-    var browsingData = storedData.browsingData || {
+    var DAILY_TIME_LIMIT_MS = 10000 * 30;
+    var dkey = getDateKey();
+    var browsingData = storedData.browsingData || {};
+    clearOlderEntries(browsingData, dkey);
+
+    setValueIfNotExists(browsingData, dkey, {
         currentUrl: undefined,
         browseStart: undefined,
-        dateKey: getUTCDateKey(),
         timeSpentMs: 0
+    });
+    browsingData = browsingData[dkey];
+
+    function storeBrowsingData() {
+        var store = {};
+        store[getDateKey()] = browsingData;
+        chrome.storage.local.set({browsingData:store});
     };
-    
-    if (!('dateKey' in browsingData)) {
-        browsingData.dateKey = getUTCDateKey();
-    }
 
-    if (!('browseStart' in browsingData)) {
-        browsingData.browseStart = undefined;
-    }
-    if (!('timeSpentMs' in browsingData)) {
-        browsingData.timeSpentMs = 0;
-    }
-
+    var redirectUrl = "chrome://newtab";
     // All the browsing data logic is in here.
     function processTabEvent(newTab) {
         // First, update browsing data.
@@ -114,6 +149,9 @@ function initialize(storedData) {
         browsingData.currentUrl = newurl;
 
         var f = isWikipediaDomain;
+
+        // Cancel the closer alarm.
+        chrome.alarms.clear("closer");
 
         // update time spent so far.
         if (f(oldurl) && !f(newurl)) {
@@ -125,21 +163,45 @@ function initialize(storedData) {
 
         if (f(newurl)) {
             if (browsingData.timeSpentMs >= DAILY_TIME_LIMIT_MS) {
-                var redirectUrl = "chrome://newtab";
                 chrome.tabs.update(newTab.id, {url:redirectUrl});
                 alert("quota over, redirecting to " + redirectUrl);
             } else {
                 // Create an alarm for the remaining time.
                 var timeRemainingMs = DAILY_TIME_LIMIT_MS - browsingData.timeSpentMs;
+                chrome.alarms.create("closer", {
+                    when: new Date().getTime() + timeRemainingMs
+                });
             }
         }
 
-        chrome.storage.local.set({'browsingData':browsingData});
+        // Sync browsing data to local storage.
+        storeBrowsingData();
     };
 
-    // Listen for tab events like create/update/remove and update user browsing info
-    // accordingly.
+    // Listen for tab events like create/update/remove and update user browsing info.
     addTabEventListeners(processTabEvent);
+
+    // Set a recurring alarm for midnight handling and register a handler for it.
+    var nextMidNightTime = getNextMidnightTime();
+    chrome.alarms.create("midnighter", {
+        when: nextMidNightTime.getTime(),
+        periodInMinutes: 24 * 60
+    });
+
+    chrome.alarms.onAlarm.addListener(function(alarm) {
+        if (alarm.name === "midnighter") {
+            // Reset the daily limit.
+            browsingData.timeSpentMs = 0;
+            browsingData.currentUrl = undefined;
+            browsingData.browseStart = undefined;
+
+            // Sync browsing data to local storage.
+            storeBrowsingData();
+        } else if (alarm.name === "closer")  {
+            // Redirect the user to redirect page.
+            chrome.tabs.update({url:redirectUrl});
+        }
+    });
 };
 
 chrome.storage.local.get(['browsingData'], initialize);
